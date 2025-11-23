0d2cf39c7827b0ea22b790d2f8fb6b081bac8a0f
bestdmit <bestdmit@gmail.com>
2025-11-23 08:26:37 +0300

    backend and frontend start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-<<<<<<< Updated upstream
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-=======
 import React from "react";
 import { Layout } from "antd";
 import TableSpecialists from "./tableOfSpecialists";
@@ -39,46 +37,39 @@ const layoutStyle: React.CSSProperties = {
   margin: 0,
   padding: 0,
 };
->>>>>>> Stashed changes
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<<<<<<< Updated upstream
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
     <Layout style={layoutStyle}>
       <AppHeader></AppHeader>
       <Content style={contentStyle}>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
         <TableSpecialists />
       </Content>
       <Footer style={footerStyle}>Footer</Footer>
     </Layout>
   );
->>>>>>> Stashed changes
 }
 
 export default App
